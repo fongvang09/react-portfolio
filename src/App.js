e713180c37b67e5edb146a374cf7601236987af5
fongvang09 <koichi_vang@hotmail.com>
2021-03-09 22:46:47 -0600

    saving final work. testing out hashrouter etc

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./pages/Portfolio.css";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import { Route } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // import { HashRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -20,11 +20,13 @@ function App() {
           <Header />
           <Wrapper>
             {/* <Link exact path ="/" component={Home} /> */}
+
             <Route exact path ="/" component={Home} />
             <Route exact path ="/index" component={Home} />
             <Route exact path ="/contact" component={Contact} />
             <Route exact path ="/portfolio" component={Portfolio} />
-            {/* <Link to="/" component={Home} />
+
+            {/* <Link to="/#" component={Home} />
             <Link to="/index" component={Home} />
             <Link to="/contact" component={Contact} />
             <Link to="/portfolio" component={Portfolio} /> */}
